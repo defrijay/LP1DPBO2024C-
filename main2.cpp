cbf62f53f86a98ede616e9c379adbbdf3b487869
defrijay <120453139+defrijay@users.noreply.github.com>
2024-02-07 11:57:36 +0700

    Add files via upload
    testes

--- main2.cpp
+++ main2.cpp
@@ -8,15 +8,15 @@ int main()
     DPR dpr;
 
     // Menambahkan beberapa anggota DPR
-    dpr.tambahAnggotaDPR(AnggotaDPR(1, "John Doe", "Hukum", "Partai A"));
-    dpr.tambahAnggotaDPR(AnggotaDPR(2, "Jane Smith", "Keuangan", "Partai B"));
+    dpr.tambahAnggotaDPR(AnggotaDPR(1, "Miles", "Hukum", "Partai A"));
+    dpr.tambahAnggotaDPR(AnggotaDPR(2, "Morales", "Keuangan", "Partai B"));
 
     // Menampilkan anggota DPR
     cout << "Daftar Anggota DPR:" << endl;
     dpr.tampilkanAnggotaDPR();
 
     // Mengubah data anggota DPR dengan ID 1
-    dpr.ubahAnggotaDPR(1, "John Doe", "Pendidikan", "Partai A");
+    dpr.ubahAnggotaDPR(1, "Peter", "Pendidikan", "Partai A");
 
     // Menampilkan anggota DPR setelah diubah
     cout << "\nDaftar Anggota DPR setelah perubahan:" << endl;
